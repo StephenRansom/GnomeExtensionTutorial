a1534bd2f8c708627b37d5d82445dc21168f0246
StephenRansom <stephen.ransom@pm.me>
2020-07-19 13:38:15 -0600

    middle of part two updates

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 Based off the "Just Perfection" gnome extension tutorials
 
 
-To install, drop into ~/.local/share/gnome-shell/extensions/desgin.scratchpad/
+##Installation
+- drop files into ~/.local/share/gnome-shell/extensions/gnome-extension-button@design.scratchpad
+- If using x.org, restart it
+    - press alt+f2
+    - type r
+    - press enter
+- If using wayland, log out then log back in
+- enable the extension in gnome tweaks
--- gnomeExtensionButton/extension.js
+++ gnomeExtensionButton/extension.js
@@ -1,7 +1,15 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
+const Mainloop = imports.mainloop;
+const GLib = imports.gi.GLib;
 
-const panelButton, panelButtonText;
+let panelButton, panelButtonText, timeout;
+
+function setButtonText() {
+  var [ok, out, err, exit] = GLib.spawn_command_line_sync('');
+  panelButtonText.set_text(out.toString());
+  return true;
+}
 
 function init(){
   panelButton = new St.bin({
@@ -18,8 +26,10 @@ function init(){
 
 function enable(){
   Main.panel._rightBox.insert_child_at_index(panelButton, 1);
+  timeout = Mainloop.timeout_add_seconds(1.0, setButtonText);
 }
 
 function disable(){
+  Mainloop.source_remove(timeout);
   Main.panel._rightBox.remove_child(panelButton);
 }
